cb9eae3a3cb44dff45234742ca91cf8159d094ee
u68 <78559138+u68@users.noreply.github.com>
2024-02-17 18:26:06 +1300

    Update main.py
    big update now supports hex or asm input uses 2 disassembly files.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from tkinter import filedialog  # This imports filedialog module from tkinter
+from tkinter import filedialog
 
-def remove_newline(strings):  # This defines a function to remove newline characters from strings
+def remove_newline(strings):
     return [string.replace('\n', '') for string in strings]
 
-def extract_lines_before_pop_pc(file_path):  # This function extracts lines before "pop pc" from a file
+def extract_lines_before_pop_pc(file_path):
     lines_before_pop_pc = []
     with open(file_path, 'r') as file:
         previous_line = ''
@@ -14,24 +14,21 @@ def extract_lines_before_pop_pc(file_path):  # This function extracts lines befo
             previous_line = line
     return lines_before_pop_pc
 
-def find_matching_strings(list1, list2):  # This function finds matching strings between two lists
+def find_matching_strings(list1, list2, list3):
     result = []
     for string1 in list1:
-        found_match = False
-        for string2 in list2:
-            if string1 in string2:
-                # Extracting characters 34th to 39th and formatting them
-                extracted_chars = string2[33:39]
+        match_info = ''
+        for string3 in list3:
+            if string1 in string3:
+                extracted_chars = string3[33:39]
                 formatted_chars = extracted_chars[3:5] + ' ' + extracted_chars[1:3] + ' ' + 'x' + extracted_chars[0] + ' xx'
                 result.append(formatted_chars)
-                found_match = True
-                break  # Stop processing list2 after finding a match
-        if not found_match:
-            # If no match was found in list2 for string1
-            result.append("No match found for {}".format(string1))
+                break
+        else:
+            result.append(f"No match found for {string1}")
     return result
 
-def open_rop_file():  # This function opens a file dialog to select a ROP file
+def open_rop_file():
     file_path = filedialog.askopenfilename()
     if file_path:
         global ropfile
@@ -41,7 +38,7 @@ def open_rop_file():  # This function opens a file dialog to select a ROP file
             rop_content = remove_newline(file.readlines())
             rop_text.insert(tk.END, '\n'.join(rop_content))
 
-def open_disas_file():  # This function opens a file dialog to select a disassembly file
+def open_disas_file():
     file_path = filedialog.askopenfilename()
     if file_path:
         global disasfile
@@ -51,14 +48,32 @@ def open_disas_file():  # This function opens a file dialog to select a disassem
             disas_content = remove_newline(file.readlines())
             disas_text.insert(tk.END, '\n'.join(disas_content))
 
-def compile_translate():  # This function compiles and translates the ROP and disassembly files
+def open_disasb_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        global disasbfile
+        disasbfile = file_path
+        disasb_text.delete("1.0", tk.END)
+        with open(disasbfile, 'r') as file:
+            disasb_content = remove_newline(file.readlines())
+            disasb_text.insert(tk.END, '\n'.join(disasb_content))
+
+def compile_translate():
+    if not ropfile or not disasfile or not disasbfile:
+        result_text.delete("1.0", tk.END)
+        result_text.insert(tk.END, "Please select ROP file, Disassembly file, and Disassembly B file.")
+        return
+    
     rop = remove_newline(open(ropfile).readlines())
-    lines_before_pop_pc = extract_lines_before_pop_pc(disasfile)
-    matching_strings = find_matching_strings(rop, lines_before_pop_pc)
+    disas_lines_before_pop_pc = extract_lines_before_pop_pc(disasfile)
+    disasb_lines_before_pop_pc = extract_lines_before_pop_pc(disasbfile)
+    
+    matching_strings = find_matching_strings(rop, disas_lines_before_pop_pc, disasb_lines_before_pop_pc)
+    
     result_text.delete("1.0", tk.END)
     result_text.insert(tk.END, '\n'.join(matching_strings))
 
-def about_popup():  # This function creates a popup window displaying information about the program
+def about_popup():
     popup = tk.Toplevel(root)
     popup.title("AnyRop-Translator")
     
@@ -66,70 +81,70 @@ def about_popup():  # This function creates a popup window displaying informatio
     readme_content = ''
     with open('version', 'r') as version_file:
         version_content = version_file.read()
-    with open('readme.md', 'r') as readme_file:
+    with open('README.md', 'r') as readme_file:
         readme_content = readme_file.read()
 
     lines = readme_content.split('\n')
     for line in lines:
-        if line.startswith('#'):  # This creates a label with big font size for lines starting with '#'
+        if line.startswith('#'):
             label = tk.Label(popup, text=line[1:], font=("Arial", 16, "bold"))
             label.pack(padx=10, pady=10)
-        elif line.startswith('__') and line.endswith('__'):  # This creates a bold label for lines enclosed in '__'
+        elif line.startswith('__') and line.endswith('__'):
             label = tk.Label(popup, text=line[2:-2], font=("Arial", 12, "bold"))
             label.pack(padx=10, pady=2)
-        else:  # This creates a label for other lines
+        else:
             label = tk.Label(popup, text=line)
             label.pack(padx=10, pady=2)
 
 root = tk.Tk()
-root.title("AnyRop Translator")  # This sets the title of the main window
+root.title("AnyRop Translator")
 
-# Create menu bar
 menubar = tk.Menu(root)
 root.config(menu=menubar)
 
-# Create File menu
 file_menu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Open ROP File", command=open_rop_file)
 file_menu.add_command(label="Open Disas File", command=open_disas_file)
+file_menu.add_command(label="Open DisasB File", command=open_disasb_file)
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 
-# Create Compile menu
 compile_menu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Compile", menu=compile_menu)
 compile_menu.add_command(label="Translate", command=compile_translate)
 
-# Create About menu
 about_menu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="About", menu=about_menu)
 about_menu.add_command(label="About", command=about_popup)
 
-# Set up grid
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 root.rowconfigure(1, weight=1)
 root.rowconfigure(3, weight=1)
 
-# Create labels and text boxes
 rop_label = tk.Label(root, text="ROP File:")
 rop_label.grid(row=0, column=0, padx=5, pady=5, sticky='w')
 
 disas_label = tk.Label(root, text="Disas File:")
 disas_label.grid(row=0, column=1, padx=5, pady=5, sticky='w')
 
-rop_text = tk.Text(root, height=10, width=50)
+disasb_label = tk.Label(root, text="DisasB File:")
+disasb_label.grid(row=0, column=2, padx=5, pady=5, sticky='w')
+
+rop_text = tk.Text(root, height=10, width=30)
 rop_text.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
 
-disas_text = tk.Text(root, height=10, width=50)
+disas_text = tk.Text(root, height=10, width=30)
 disas_text.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
+
+disasb_text = tk.Text(root, height=10, width=30)
+disasb_text.grid(row=1, column=2, padx=5, pady=5, sticky='nsew')
 
 result_label = tk.Label(root, text="Output:")
 result_label.grid(row=2, column=0, padx=5, pady=5, sticky='w')
 
 result_text = tk.Text(root, height=20, width=100)
-result_text.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky='nsew')
+result_text.grid(row=3, column=0, columnspan=3, padx=5, pady=5, sticky='nsew')
 
-root.mainloop()  # This starts the main event loop
-
+root.mainloop()
